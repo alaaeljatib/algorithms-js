53ac2d5ec4d4fee2ef942e6c3eea838b32b2d9a9
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-01-10 23:43:14 -0500

    Add Find Loop in LinkedList

---
+++ findLoopInLinkedList.js
@@ -0,0 +1,46 @@
+const LinkedList = require('./LinkedList');
+
+const findLoopInLinkedList = (lst) => {
+    if (lst.head === null) return null;
+    let slow = lst.head,
+        fast = lst.head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) break;
+    }
+
+    if (fast === null || fast.next === null) return null;
+
+    slow = lst.head;
+
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return fast;
+}
+
+const lst = new LinkedList(0);
+lst.addNode
+const generateLoopedLinkedList = (lst, maxVal) => {
+    let pointer = lst.head;
+
+    for (let i = 0; i < maxVal; i++) {
+        lst.addNode(i);
+        pointer = pointer.next;
+    }
+    pointer.next = lst.head.next.next.next.next.next;
+    return lst;
+}
+
+module.exports = findLoopInLinkedList;
+
+// const loopedList = generateLoopedLinkedList(lst, 20);
+
+
+
+
+// console.log('Looped list is ', lst, ' looping is at index number ', findLoopInLinkedList(loopedList).value);
