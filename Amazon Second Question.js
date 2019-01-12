9c4537ff24cc28605b1370b5e75b8dcb5c4aa35e
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-01-12 11:35:03 -0500

    Updated version of Amazon Second Problem

--- Amazon Second Question.js
+++ Amazon Second Question.js
@@ -1,9 +1,4 @@
-// IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
-// SOME FUNCTIONALITY WITHIN A PACKAGE MAY BE RESTRICTED
-// DEFINE ANY FUNCTION NEEDED
-// FUNCTION SIGNATURE BEGINS, THIS FUNCTION IS REQUIRED
-function minimumDistance(numRows, numColumns, area) {
-    // WRITE YOUR CODE HERE
+const minimumDistance = (area, rows, cols) => {
 
     if (area === null || (area !== null && area.length === 0)) return -1;
 
@@ -11,56 +6,60 @@ function minimumDistance(numRows, numColumns, area) {
 
     if (arr.length === 1 && arr[0].length === 2 && arr[0][0] === 1 && arr[0][1] === 9) return 1;
 
-    let toCheckQueue = []; // use array as queue where push first pop last
+    const toCheckQueue = [];
 
-    toCheckQueue.unshift([0, 0]);
+    const currentCost = [];
 
-    let currentCost = [];
-
-    let prev = [];
-
-    for (let i = 0; i < numRows; i++) {
-        const row = [];
-        for (let j = 0; j < numColumns; j++) {
-            row.push(0);
+    // Initiating the arrays before starting to allocate the N location in memory
+    for (let i = 0; i < rows; i++) {
+        const currentRow = [];
+        for (let j = 0; j < cols; j++) {
+            currentRow.push([0, false]);
         }
-        currentCost.push(row);
-        prev.push(row);
+        currentCost.push(currentRow);
     }
 
+    // Let the fun begin!
+    toCheckQueue.unshift([0, 0]);
 
     while (toCheckQueue.length > 0) {
-        const [toCheckRow, toCheckCol] = toCheckQueue.pop();
 
-        currentCost[toCheckRow][toCheckCol] = area[toCheckRow][toCheckCol] + prev[toCheckRow][toCheckCol];
+        const [toCheckRow, toCheckCol] = toCheckQueue.pop();
+        // console.log('queue', toCheckQueue, 'toCheckRow', toCheckRow, 'toCheckCol', toCheckCol);
 
         for (let i = toCheckRow - 1; i <= toCheckRow + 1; i++) {
-            for (let j = toCheckCol - 1; j <= toCheckCol; j++) {
-                if (i >= 0 && j >= 0 && i < numColumns && j < numRows) {
+            for (let j = toCheckCol - 1; j <= toCheckCol + 1; j++) {
+                if (i >= 0 && j >= 0 && i < rows && j < cols && (i === toCheckRow ^ j === toCheckCol) && !currentCost[i][j][1]) {
+                    console.log(i, j, toCheckRow, toCheckCol);
+
                     const currentAreaValue = area[i][j];
 
                     if (currentAreaValue === 1) {
-                        if (currentAreaValue + prev[i][j] < currentCost[i][j]) {
-                            currentCost[i][j] = currentAreaValue + prev[i][j];
-                            toCheckQueue.unshift([i, j]);
-                        } else if (currentAreaValue === 9) { // found it!
-                            currentCost[i][j] = currentAreaValue + Math.min(prev[i - 1][j], prev[i][j - 1], prev[i - 1][j - 1], prev[i][j]);
-                            return current[i][j];
+                        // console.log('curretValue is 1');
+                        if (toCheckRow === 0 && toCheckCol === 0) {
+                            currentCost[i][j] = [currentAreaValue, true];
+                        } else {
+                            // console.log('prev is not empty');
+                            currentCost[i][j] = [currentAreaValue + currentCost[toCheckRow][toCheckCol][0], true];
+
+                            console.log('curentCost for i j ', i, j, currentCost[i][j]);
                         }
+                        toCheckQueue.unshift([i, j]);
+                    } else if (currentAreaValue === 9) { // found it!
+                        return currentCost[toCheckRow][toCheckCol][0];
+
                     }
                 }
             }
-
-            prev = currentCost.slice();
         }
-        return -1;
     }
+    return -1;
 }
-// FUNCTION SIGNATURE ENDS
 
 const arr = [
     [1, 0, 1],
     [1, 1, 1],
     [1, 9, 1]
 ]
-console.log(minimumDistance(3, 3, arr));
+
+console.log(minimumDistance(arr, 3, 3));
